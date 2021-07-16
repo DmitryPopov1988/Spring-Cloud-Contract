18069ba718f18e4e1320b3be6ea30cab5458b9bb
Dmitry Popov <dmitry.popov@godel.shellenergy.co.uk>
2021-07-16 10:20:49 +0300

    add bodyMatchers

--- src/test/resources/contracts/shouldSendFlow.groovy
+++ src/test/resources/contracts/shouldSendFlow.groovy
@@ -19,21 +19,13 @@ Contract.make {
         body(file('UT003.xml').toString())
 
 
-//        Example to verify payload in XML
-//        bodyMatchers {
-//            xPath('/test/duck/text()', byRegex("[0-9]{3}"))
-//            xPath('/test/duck/text()', byCommand('equals($it)'))
-//            xPath('/test/duck/xxx', byNull())
-//            xPath('/test/duck/text()', byEquality())
-//            xPath('/test/alpha/text()', byRegex(onlyAlphaUnicode()))
-//            xPath('/test/alpha/text()', byEquality())
-//            xPath('/test/number/text()', byRegex(number()))
-//            xPath('/test/date/text()', byDate())
-//            xPath('/test/dateTime/text()', byTimestamp())
-//            xPath('/test/time/text()', byTime())
-//            xPath('/test/*/complex/text()', byEquality())
-//            xPath('/test/duck/@type', byEquality())
-//        }
-
+        bodyMatchers {
+            xPath('/formfillXML/header/ORIGINATORID/text()', byRegex(onlyAlphaUnicode()))
+            xPath('/formfillXML/header/ORIGINATORROLECODE/text()', byRegex(onlyAlphaUnicode()))
+            xPath('/formfillXML/header/CREATEDDATE/text()', byRegex('[0-9]{6}'))
+            xPath('/formfillXML/header/CREATEDTIME/text()', byRegex('[0-9]{6}'))
+            xPath('/formfillXML/body/record/K0533/text()', byRegex(number()))
+        }
     }
+
 }
